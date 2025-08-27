583c98046e1f50b548ab6fa7798067a800554b88
epkasilag1 <kasilageugene@gmail.com>
2025-08-27 21:39:25 +0800

    add player name and scores

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,24 @@ function App() {
 
   return (
     <>
-      <div className="flex items-center justify-center h-[100vh]">
+      <div className="flex items-center justify-center h-[100vh] w-full overflow-hidden bg-gray-100">
+        <Player name="Eugene" />   
         <Board />
+        <Player />
       </div>
     </>
+  )
+}
+
+function Player({name}) {
+  const player = name ? name : "Computer";
+  const [score, setScore] = useState(0);
+
+  return (
+    <div className='text-center text-sm md:text-2xl font-semibold'>
+      <h1>{player}</h1>
+      <h2 className="text-2xl md:text-6xl">{score}</h2>
+    </div>
   )
 }
 
@@ -25,11 +39,11 @@ function Board() {
   }
 
   return (
-    <div className='flex items-center justify-center'>
+    <div className='flex items-center p-4 bg-white mx-4 rounded-lg border'>
       <div className="grid grid-cols-3 gap-1 md:gap-2">
         {
           cells.map((num, index) => (
-            <button className="border border-gray-500 bg-gray-400 text-white text-3xl w-24 h-24 md:w-42 md:h-42 rounded-lg hover:bg-gray-600 active:bg-gray-800" key={index} onClick={() => updateCell(index)}>{num}</button>
+            <button className="border border-gray-500 bg-gray-400 text-white text-3xl w-12 h-12 sm:w-16 sm:h-16 md:w-32 md:h-32 md:text-3xl rounded-lg hover:bg-gray-600 active:bg-gray-800" key={index} onClick={() => updateCell(index)}>{num}</button>
           ))
         }
       </div>
