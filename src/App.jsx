0f4c92a5e50ab5daee0fd39074745523cd985764
epkasilag1 <kasilageugene@gmail.com>
2025-08-27 22:45:41 +0800

    alternating player turn

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,51 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const playerOneScore = 0;
+  const playerTwoScore = 0;
 
   return (
     <>
       <div className="flex items-center justify-center h-[100vh] w-full overflow-hidden bg-gray-100">
-        <Player name="Eugene" />   
+        <Player name="Player" playerScore={playerOneScore} />   
         <Board />
-        <Player />
+        <Player playerScore={playerTwoScore}/>
       </div>
     </>
   )
 }
 
-function Player({name}) {
+function Player({name, playerScore}) {
   const player = name ? name : "Computer";
-  const [score, setScore] = useState(0);
 
   return (
-    <div className='text-center text-sm md:text-2xl font-semibold'>
+    <div className='text-center text-sm md:text-2xl font-semibold w-16 md:w-32'>
       <h1>{player}</h1>
-      <h2 className="text-2xl md:text-6xl">{score}</h2>
+      <h2 className="text-2xl md:text-6xl">{playerScore}</h2>
     </div>
   )
 }
 
 function Board() {
 
-  const [cells, setCell] = useState(Array(9).fill(0))
+  const [cells, setCell] = useState(Array(9).fill(null))
 
-  const updateCell = (index) => {
+  const [turn, setTurn] = useState(true)
+
+  const updateCell = (index, playerTurn) => {
     setCell((prevCells) => {
       const newCells = [...prevCells]
-      newCells[index] += 1
+      if (turn != playerTurn & !newCells[index]){
+        newCells[index] = turn ? "X" : "O"
+        nextTurn()
+      }
       return newCells
+    })
+  }
+
+  const nextTurn = () => {
+    setTurn(() => {
+      return !turn
     })
   }
 
