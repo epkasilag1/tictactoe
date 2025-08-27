7a98651885341b376f86f141c385669313239a85
epkasilag1 <kasilageugene@gmail.com>
2025-08-28 00:00:17 +0800

    add background image

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const playerOneScore = 0;
-  const playerTwoScore = 0;
-
+  const [playerScore, setScore] = useState([0, 0]);
+  const name = "Player";
   return (
     <>
-      <div className="flex items-center justify-center h-[100vh] w-full overflow-hidden bg-gray-100">
-        <Player name="Player" playerScore={playerOneScore} />   
-        <Board />
-        <Player playerScore={playerTwoScore}/>
+      <div className="flex items-center justify-center h-[100vh] w-full overflow-hidden select-none">
+        <Player name={name} playerScore={playerScore} />   
+        <Board name={name} />
+        <Player playerScore={playerScore}/>
       </div>
     </>
   )
@@ -21,13 +20,14 @@ function Player({name, playerScore}) {
 
   return (
     <div className='text-center text-sm md:text-2xl font-semibold w-16 md:w-32'>
+      <h1 className= {`text-bold text-5xl ${name ? "text-red-500" : "text-green-700"}`}>{name ? "X" : "O"}</h1>
       <h1>{player}</h1>
-      <h2 className="text-2xl md:text-6xl">{playerScore}</h2>
+      <h2 className="text-2xl md:text-3xl mt-2">{name ? playerScore[0] : playerScore[1]}</h2>
     </div>
   )
 }
 
-function Board() {
+function Board({name}) {
 
   const [cells, setCell] = useState(Array(9).fill(null))
 
@@ -51,13 +51,16 @@ function Board() {
   }
 
   return (
-    <div className='flex items-center p-4 bg-white mx-4 rounded-lg border'>
-      <div className="grid grid-cols-3 gap-1 md:gap-2">
-        {
-          cells.map((num, index) => (
-            <button className="border border-gray-500 bg-gray-400 text-white text-3xl w-12 h-12 sm:w-16 sm:h-16 md:w-32 md:h-32 md:text-3xl rounded-lg hover:bg-gray-600 active:bg-gray-800" key={index} onClick={() => updateCell(index)}>{num}</button>
-          ))
-        }
+    <div className='flex flex-col items-center'>
+      <h1 className="text-3xl mb-2"><span className='font-semibold'>{turn ? name : "Computer"}</span>'s turn</h1>
+      <div className='flex items-center p-1 md:p-2 bg-white/70 mx-4 rounded-lg border'>
+        <div className="grid grid-cols-3 gap-1 md:gap-2">
+          {
+            cells.map((num, index) => (
+              <button className={`border border-gray-500 bg-gray-300 text-3xl font-bold w-12 h-12 sm:w-16 sm:h-16 md:w-32 md:h-32 md:text-3xl rounded-lg hover:bg-gray-600 active:bg-gray-800 ${cells[index]=="X" ? "text-red-500" : "text-green-700"}`} key={index} onClick={() => updateCell(index)}>{num}</button>
+            ))
+          }
+        </div>
       </div>
     </div>
   )
