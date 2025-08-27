532b4644071c0843a0f48536bba284ab7f73422e
epkasilag1 <kasilageugene@gmail.com>
2025-08-27 18:33:00 +0800

    add 3x3 grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex items-center justify-center h-[100vh]">
+        <Board />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
+  )
+}
+
+function Board() {
+
+  const [cells, setCell] = useState(Array(9).fill(0))
+
+  const updateCell = (index) => {
+    setCell((prevCells) => {
+      const newCells = [...prevCells]
+      newCells[index] += 1
+      return newCells
+    })
+  }
+
+  return (
+    <div className='flex items-center justify-center'>
+      <div className="grid grid-cols-3 gap-1 md:gap-2">
+        {
+          cells.map((num, index) => (
+            <button className="border border-gray-500 bg-gray-400 text-white text-3xl w-24 h-24 md:w-42 md:h-42 rounded-lg hover:bg-gray-600 active:bg-gray-800" key={index} onClick={() => updateCell(index)}>{num}</button>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
